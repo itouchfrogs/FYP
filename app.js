6bd38c1b70044281da00dd050fd0192858f14629
frog toucher <maxwelljakarta7@gmail.com>
2026-07-26 16:31:34 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1759,7 +1759,7 @@ app.post("/admins/add", requireAdmin, async (req, res) => {
 
         // Validate role
         if (role !== 'admin' && role !== 'employee') {
-            return res.status(400).send("Role must be 'admin' or 'player'");
+            return res.status(400).send("Role must be 'admin' or 'employee'");
         }
 
         // Check if username already exists
